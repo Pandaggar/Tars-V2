fcbb2d252a7a7e5b9565e7cccbfb73f3341daf5c
Pandaggar <Pandag3006@gmail.com>
2025-07-02 02:07:24 -0400

    night1

--- include/servoMotor.h
+++ include/servoMotor.h
@@ -1,9 +1,11 @@
+#include <Servo.h>
 
 class customServo
 {
 private:
     int servoPos;
     int servoPin;
+    Servo myservo;
 public:
     customServo(int Pin,int Pos){
         servoPin = Pin;
@@ -14,4 +16,19 @@ public:
 
     int getServoPos(){return servoPos;}
     void setServoPos(int pos){servoPos = pos;}
+
+    void linear(int newPos){
+        if(newPos <= servoPos){
+            for (int pos = servoPos; pos >= newPos; pos--) {
+                myservo.write(servoPin, pos);// set the servo position (degrees)
+                delay(10);
+            }
+        }else{
+            for (int pos = servoPos; pos <= newPos; pos++) {
+                myservo.write(servoPin, pos);// set the servo position (degrees)
+                delay(10);
+            }
+        }
+        servoPos = newPos;
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include "servoMotor.h"
 const int servoPin1 = 18;
 int s1Pos;
 Servo myservo = Servo();
-
+customServo cusServo1 = customServo(servoPin1,0);
 void translate(int servoPin, int position);
 
 void setup() {
@@ -11,20 +12,15 @@ void setup() {
 }
 
 void loop() {
-  for (int pos = 0; pos <= 185; pos++) {  // go from 0-180 degrees
-    myservo.write(servoPin1, pos);        // set the servo position (degrees)
-    delay(5);
-  }
-  for (int pos = 185; pos >= 0; pos--) {  // go from 180-0 degrees
-    myservo.write(servoPin1, pos);        // set the servo position (degrees)
-    delay(5);
-  }
   delay(200);
-  myservo.write(servoPin1,90);
-  delay(200);
-  myservo.write(servoPin1,0);
-  delay(200);
+  Serial.println(cusServo1.getServoPos());
+  cusServo1.linear(45);
+  Serial.println(cusServo1.getServoPos());
+  cusServo1.linear(180);
+  Serial.println(cusServo1.getServoPos());
+  cusServo1.linear(90);
 }
+
 
 void translate(int servoPin, int position){
   for (int pos = s1Pos; pos <= 185; pos++) {  // go from 0-180 degrees
