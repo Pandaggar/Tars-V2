1f96f2290f884c047afd4e3df10311bea91d59d9
Pandaggar <Pandag3006@gmail.com>
2025-07-03 13:56:04 -0400

    Movement List Functionality Commit

--- include/servoMotor.h
+++ include/servoMotor.h
@@ -1,16 +1,34 @@
+#pragma once
 #include <Arduino.h>
 #include <ESP32_Servo.h>
+
+#define Max_Moves 10
 
 class customServo
 {
 private:
+    struct Move{
+        int speed;
+        int position;
+    };
+
     int servoPos;
     int servoPin;
     Servo myServo;
+
+    // Non-blocking movement
+    int targetPos;
+    int spd;
+    unsigned long lastUpdate = 0;
+    bool moving = false;
+
+    //  movement list
+    Move movements[Max_Moves];
+    int first = 0;
+    int last = 0;
 public:
-    customServo(int Pin,int Pos){
+    customServo(int Pin){
         servoPin = Pin;
-        servoPos = Pos;
     };
 
     int getServoPin(){return servoPin;}
@@ -18,22 +36,53 @@ public:
     int getServoPos(){return servoPos;}
     void setServoPos(int pos){servoPos = pos;}
 
+    bool isMoving() {return (moving || (first != last));}
+
     void attach(){
         myServo.attach(servoPin);
+        myServo.write(90);
+        targetPos = 90;
+        servoPos = 90;
     }
 
-    void linear(int newPos,int speed = 10){
-        if(newPos <= servoPos){
-            for (int pos = servoPos; pos >= newPos; pos--) {
-                myServo.write(pos);// set the servo position (degrees)
-                delay(speed);
+    void linear(int newPos,int speed = 15){
+        int next = (last + 1) % Max_Moves;
+        if (next != first) {  // Check if list is not full
+            movements[last].position = newPos;
+            movements[last].speed = speed;
+            last = next;
+        } else {
+            // list full
+        }
+    }
+
+    void update(){
+        if(!moving){
+            //seeing if there is a move in the list
+            if(first != last){
+                targetPos = movements[first].position;
+                spd = movements[first].speed;
+                first = (first + 1) % Max_Moves;
+                moving = true;
+                lastUpdate = millis();
             }
-        }else{
-            for (int pos = servoPos; pos <= newPos; pos++) {
-                myServo.write(pos);// set the servo position (degrees)
-                delay(speed);
+            return;
+        }
+
+        unsigned long currentMillis = millis();
+        if (currentMillis - lastUpdate >= spd){
+            lastUpdate = currentMillis;
+
+            if(targetPos < servoPos){
+                servoPos--;
+            }else if(targetPos > servoPos){
+                servoPos++;
+            }   
+            myServo.write(servoPos);
+
+            if (servoPos == targetPos) {
+                moving = false;
             }
         }
-        servoPos = newPos;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 #include <ESP32_Servo.h>
 #include "servoMotor.h"
+int stage = 0;
 const int servoPin1 = 19;
 const int servoPin2 = 18;
 const int servoPin3 = 5;
-customServo Servo1 = customServo(servoPin1,90);
-customServo Servo2 = customServo(servoPin2,90);
-customServo Servo3 = customServo(servoPin3,90);
+customServo Servo1 = customServo(servoPin1);
+customServo Servo2 = customServo(servoPin2);
+customServo Servo3 = customServo(servoPin3);
 
 void setup() {
   Serial.begin(9600);
@@ -16,11 +17,31 @@ void setup() {
 }
 
 void loop() {
-  Servo1.linear(0,5);
-  Servo1.linear(200,5);
-  Servo2.linear(0);
-  Servo2.linear(180);
-  Servo3.linear(0,100);
-  Servo3.linear(180,100);
-  delay(200);
+  if(stage == 0){
+    Servo1.linear(0);
+    Servo1.linear(180);
+    Servo2.linear(0);
+    Servo2.linear(90);
+    Servo3.linear(0);
+    Servo3.linear(180);
+    Servo3.linear(90);
+    stage =1;
+  }if(stage == 3){
+    stage = 0;
+  }
+  if(stage == 1){
+    Servo1.update();
+    Servo2.update();
+    if(!Servo1.isMoving()){
+    Serial.print("stage2 \t");
+    stage = 2;
+    }
+  }
+  if(stage == 2){
+    Servo3.update();
+    if(!Servo3.isMoving()){
+    Serial.println("stage3 \t");
+    stage = 3;
+    }
+  }
 }
